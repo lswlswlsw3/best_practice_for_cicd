e77bcea77eed72dd6243ad9f939dccbc66fd36eb
mzc01-seonwoong <mzc01-seonwoong@10.90.8.12>
2020-06-11 13:20:58 +0900

    first init commit for ci/cd

---
+++ init-test/src/main/java/com/example/demo/apj/InitRestController.java
@@ -0,0 +1,24 @@
+package com.example.demo.apj;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * init RestConroller
+ * @author mzc01-seonwoong
+ * @since 20.06.11
+ */
+
+@RestController
+@RequestMapping("/init")
+public class InitRestController {
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello to spring boot!";
+	}
+	
+	@RequestMapping("/firstCommit")
+	public String firstCommit() {
+		return "first commit!";
+	}
+}
