2dce6a3387034aa95ca5a94483cd679499cd651b
mzc01-seonwoong <mzc01-seonwoong@169.254.34.186>
2020-06-17 16:43:35 +0900

    docker rm 추

--- init-test/src/main/java/com/example/demo/api/InitRestController.java
+++ init-test/src/main/java/com/example/demo/api/InitRestController.java
@@ -17,6 +17,6 @@ public class InitRestController {
 	@RequestMapping("")
 	public String firstCommit() {
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		return "best practice cicd works!! : "+format.format(System.currentTimeMillis());
+		return "best practice cicd works! : "+format.format(System.currentTimeMillis());
 	}
 }
